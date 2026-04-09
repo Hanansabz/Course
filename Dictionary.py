d840b9a23790b4de4db128a9e6775831aca529c1
Hanan Sabz <sabzhanan@gmail.com>
2026-04-09 23:43:53 +0300

    sys argument

--- Dictionary.py
+++ Dictionary.py
@@ -1,3 +1,5 @@
+import sys
+
 def read_file_content_to_dictionary(file_name):
     with open(file_name) as file:
         d = {}
@@ -16,10 +18,16 @@ def sort_dictionary_by_value(d, n):
     sorted_d = dict(l) # Convert the list of tuples back to a dictionary
     return sorted_d
 
-n = int(input("Enter the number of top words to display: "))
-file_name = "C://Users//Hanan//Course//Storytext.txt"
-d = read_file_content_to_dictionary(file_name)
-sorted_d = sort_dictionary_by_value(d, n)
-for key, value in sorted_d.items():
-    print("Words: " + key + " Appears " + str(value) + " times")
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        n = int(sys.argv[1])   #type into terminal: python Dictionary.py 10 to display the top 10 words for example 
+    else:
+        print("Please provide the number of top words to display as a command-line argument.")
+
+    import os
+    file_name = os.path.join(os.path.dirname(__file__), "Storytext.txt")
+    d = read_file_content_to_dictionary(file_name)
+    sorted_d = sort_dictionary_by_value(d, n)
+    for key, value in sorted_d.items():
+        print(f"Word: {key} Appears {value} times")
 
