167a114ba134308affd3b7f609241393d996e39f
Hanan Sabz <sabzhanan@gmail.com>
2026-05-13 20:35:27 +0300

    questions during the lesson

---
+++ CourseLesson.py
@@ -0,0 +1,20 @@
+## -CLASS LEARNING- ##
+
+# list_comprehension_even = [x for x in range(1, 31) if x % 2 == 0]
+# print(list_comprehension_even)
+
+# list_comprehension_3_not_4 = [x for x in range(1, 51) if x % 3 == 0 and x % 4 != 0]
+# print(list_comprehension_3_not_4)
+
+# def mystery_function(n):
+#     function = [x for x in range(1, n+1) if x % 2 == 1]
+#     return sum(function)
+
+# print(mystery_function(5))
+
+# numbers = [1, 2, 3, 4, 5]
+# print(list(map(lambda x: x*10, numbers)))
+
+# numbers = [1, 5, 10, 15, 20, 25, 30]
+# print(list(filter(lambda x: x > 10 and x % 5 == 0, numbers)))
+
