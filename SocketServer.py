02e0db4a9ff9aecca239efe94ab71e17c657cd76
Hanan Sabz <sabzhanan@gmail.com>
2026-06-01 20:05:31 +0300

    Socket Server

---
+++ SocketServer.py
@@ -0,0 +1,29 @@
+import socket
+
+def start_echo_server():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_host = 'localhost'
+    server_port = 8080
+    server_socket.bind((server_host, server_port))
+    server_socket.listen(1)
+
+    print(f"Server listening on {server_host}:{server_port}")
+
+    while True:
+        client_socket, client_address = server_socket.accept()
+        print(f"Connection established with {client_address}")
+
+        while True:
+            data = client_socket.recv(1024)
+            if not data:
+                break
+
+            message = data.decode('utf-8')
+            print(f"Received from client: {message}")
+
+            # Echo the message back to the client
+            client_socket.sendall(data)
+
+        
+        print(f"Connection with {client_address} closed")
+        client_socket.close()
